1d01a148dc3f81de6eb2e10659eb90250279eb21
Mirelly <mire.llyta226@gmail.com>
2021-07-07 21:23:52 -0500

    spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { SpinnerModule } from './components/spinner/spinner.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {TopicBanckComponent} from './components/topic-banck/topic-banck.component';
-import {MaterialModule} from './material/material.module';
-import {TopicDenunciationComponent} from './components/topic-denunciation/topic-denunciation.component';
-import {TopicProposalComponent} from './components/topic-proposal/topic-proposal.component';
-import {TopicNotificationComponent} from './components/topic-notification/topic-notification.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {ProfileComponent} from './components/profile/profile.component';
-import {AuthService} from "./services/auth.service";
-import {HttpClientModule} from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopicBanckComponent } from './components/topic-banck/topic-banck.component';
+import { MaterialModule } from './material/material.module';
+import { TopicDenunciationComponent } from './components/topic-denunciation/topic-denunciation.component';
+import { TopicProposalComponent } from './components/topic-proposal/topic-proposal.component';
+import { TopicNotificationComponent } from './components/topic-notification/topic-notification.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AuthService } from "./services/auth.service";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { LoginComponent } from './components/login/login.component';
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {FormsModule} from "@angular/forms";
-import {TopicService} from "./services/topic.service";
-import {SignUpAcademic, SignUpComponent} from './components/sign-up/sign-up.component';
-import {PaymentRegistrationComponent} from "./components/payment-registration/payment-registration.component";
-import {TopicStudentComponent} from "./components/topic-student/topic-student.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+import { TopicService } from "./services/topic.service";
+import { SignUpAcademic, SignUpComponent } from './components/sign-up/sign-up.component';
+import { PaymentRegistrationComponent } from "./components/payment-registration/payment-registration.component";
+import { TopicStudentComponent } from "./components/topic-student/topic-student.component";
+import { SpinnerInterceptor } from './components/interceptor/spinner.interceptor';
 
 
 @NgModule({
@@ -44,11 +46,13 @@ import {TopicStudentComponent} from "./components/topic-student/topic-student.co
     MaterialModule,
     HttpClientModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    SpinnerModule
   ],
   providers: [
     AuthService,
-    TopicService
+    TopicService,
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true }
   ],
   bootstrap: [AppComponent],
 })
