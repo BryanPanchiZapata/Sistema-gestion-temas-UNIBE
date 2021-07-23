0c586ea991ab0711683fedfb3cfd80a7aa786719
Mirelly <mire.llyta226@gmail.com>
2021-07-23 15:15:38 -0500

    style approval

--- src/app/components/topic-notification/topic-notification.component.ts
+++ src/app/components/topic-notification/topic-notification.component.ts
@@ -1,5 +1,6 @@
+import { TopicEvaluation } from './../../models/topic-student-model';
 import { FormControl, Validators } from '@angular/forms';
-import { TopicApprovalModel } from './../../models/topic-approval-model';
+import { TopicApprovalModel, Titles } from './../../models/topic-approval-model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TopicStudentModel } from 'src/app/models/topic-student-model';
@@ -15,29 +16,26 @@ export class TopicNotificationComponent implements OnInit {
   static END_POINT = 'topic-approval';
   public approval: TopicApprovalModel;
   public topicStudent: TopicStudentModel = {};
+  public evaluations = TopicEvaluation;
+  public titles = Titles;
 
   constructor(
     private topicStudentService: TopicStudentService,
     private route: ActivatedRoute
   ) {
-
   }
 
   ciStudentControl = new FormControl('', [
     Validators.required,
   ]);
 
+  evaluationControl = new FormControl('', [
+    Validators.required,
+  ]);
+
   ngOnInit(): void {
   }
 
-  onFindByStudent() {
-    if (this.ciStudentControl.valid)
-    this.topicStudentService.getTopicStudentByStudent(this.ciStudentControl.value).subscribe(
-      data => {
-        this.topicStudent = data;
-      }
-    )
-  }
   refresh(): void {
     window.location.reload();
   }
@@ -45,6 +43,26 @@ export class TopicNotificationComponent implements OnInit {
   onReset() {
     this.ciStudentControl.reset();
     this.refresh();
+  }
+
+  onFindByStudent() {
+    if (this.ciStudentControl.valid)
+      this.topicStudentService.getTopicStudentByStudent(this.ciStudentControl.value).subscribe(
+        data => {
+          this.topicStudent = data;
+        }
+      )
+  }
+
+
+  onEvaluationProposal() {
+    // if (this.id !== null)
+    // if (this.evaluationControl.valid)
+    //   this.topicStudentService.evaluationProposal(data.id, this.evaluationControl.value).subscribe(
+    //     data => {
+    //       this.topicStudent = data;
+    //     }
+    //   )
   }
 
   monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
--- src/app/models/topic-approval-model.ts
+++ src/app/models/topic-approval-model.ts
@@ -9,3 +9,9 @@ export interface TopicApprovalModel {
   observations?: Text,
   topicStudent: TopicStudentModel
 }
+
+export enum Titles{
+  'Señor' = '1',
+  'Señora' = '2',
+  'Señorita' = '3'
+}
