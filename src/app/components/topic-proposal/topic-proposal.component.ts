fd27f2d1d6de0cf4926a7c04aaa0d4c3aa368c62
BryanPanchiZapata <bryanlife2530@gmai.com>
2021-07-31 10:04:52 -0500

    Modal vinculos

--- src/app/components/topic-proposal/topic-proposal.component.ts
+++ src/app/components/topic-proposal/topic-proposal.component.ts
@@ -14,20 +14,6 @@ export class TopicProposalComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  /* countWords() {
-    const text = document.getElementById('count')?.innerText;
-    text = text.replace(/\r\n/g, ' ');
-    text = text.replace(/[ ]+/g, ' ');
-    text = text.replace(/^ /, '');
-    text = text.replace(/ $/, '');
-    const textSpace = text?.split(' ');
-    const number = textSpace?.length;
-    alert(number);
-    console.log(document);
-    console.log(text1);
-    console.log(textSpace);
-  } */
-
   proposalForm = this.formBuilder.group({
     objectives: ['', Validators.required],
     studyJustification: ['', Validators.required],
