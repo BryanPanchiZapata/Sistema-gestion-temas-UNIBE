dc88a0752f013927450a86928569e478eb978792
Mirelly <mire.llyta226@gmail.com>
2021-08-03 00:18:07 -0500

    contraseña

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -29,7 +29,6 @@ export class ProfileComponent implements OnInit {
     firstName: ['', Validators.required],
     email: ['', [Validators.required, Validators.pattern(this.isValidEmail)]],
     lastName: ['', Validators.required],
-    password: [[''], [ Validators.minLength(8)]],
     secondName: [''],
     secondLastname: ['']
   });
